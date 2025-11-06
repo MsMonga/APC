5cbd1b8dd5fda4c372b0ed3edd49e5de854dd03f
MsMonga <mani.cs2025@gmail.com>
2025-11-06 09:05:12 +0800

    Add files via upload

---
+++ Serial/Serial_prog1.py
@@ -0,0 +1,16 @@
+import serial
+import time
+
+ser= serial.Serial('/dev/ttyUSB0', 9600, timeout=1.0)
+time.sleep(3)
+ser.reset_input_buffer()
+print("serial ok")
+try:
+    while True:
+        time.sleep(0.01)
+        if ser.in_waiting>0:
+            line= ser.readline().decode('utf-8').rstrip()
+            print(line)
+except KeyboardInterrupt:
+    print("Close Serial Communication")
+    ser.close()
