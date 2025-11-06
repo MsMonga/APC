95884241f513cdc358df3fa70a7349099351dbbe
MsMonga <mani.cs2025@gmail.com>
2025-11-06 10:31:39 +0800

    Add files via upload

---
+++ Serial/from_Pi_arduino.ino
@@ -0,0 +1,16 @@
+void setup() {
+Serial.begin(9600);
+delay(2000);
+Serial.println("Arduino ready to receive!");
+Serial.println("Waiting for messages...");
+Serial.println("----------------------------");
+}
+
+void loop() {
+if (Serial.available() > 0) {
+String message = Serial.readStringUntil('\n');
+//Serial.print("Received: ");
+Serial.println(message);
+Serial.println("----------------------------");
+}
+}
---
+++ Serial/prog2_pi2arduino.py
@@ -0,0 +1,18 @@
+import serial
+import time
+
+ser= serial.Serial('/dev/ttyUSB0', 9600, timeout=1.0)
+time.sleep(3)
+ser.reset_input_buffer()
+print("serial ok")
+try:
+    while True:
+        time.sleep(0.01)
+        ser.write("Hello from Pi \n".encode('utf-8'))
+        while ser.in_waiting<=0:
+            time.sleep(0.01)
+        '''response = ser.readline().decode('utf-8').rstrip()
+        print(response)'''
+except KeyboardInterrupt:
+    print("Close Serial Communication")
+    ser.close()
