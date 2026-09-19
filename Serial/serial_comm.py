205690cda1027c1ac1c4eafe025f90e0733dbf25
MsMonga <mani.cs2025@gmail.com>
2026-09-19 12:14:50 +0800

    Add files via upload

---
+++ Serial/serial_comm.py
@@ -0,0 +1,25 @@
+import serial
+import time
+
+ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=0.2)   # the same 9600 as the sketch
+time.sleep(2)
+'''ser.reset_input_buffer()
+print("serial ok")
+ser.close()
+
+
+
+# the Uno resets when the port opens. Wait it out.
+'''
+print(ser.readline().decode().strip())   # the "ready" line the sketch prints
+
+try:
+    while True:
+        msg = input('> ')
+        ser.write((msg + '\n').encode())   # a line = the message, then '\n'
+        time.sleep(0.1)
+        reply = ser.readline().decode().strip()
+        if reply:
+            print(reply)
+finally:
+    ser.close() 
